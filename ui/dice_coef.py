49c786016c14cc264826cea4e3c080a3283115eb
NeroUcH <hin1997101@gmail.com>
2023-01-08 20:45:58 +0800

    style: formatting dice function

--- ui/dice_coef.py
+++ ui/dice_coef.py
@@ -9,10 +9,11 @@ def dice_coef(y_true:np.ndarray, y_pred:np.ndarray):
     return round((2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth),3)
 
 
-def dice_average_sets(y_true, y_pred):
+def dice_average_sets(y_true:np.ndarray, y_pred:np.ndarray):
     assert y_true.shape[0] == y_pred.shape[0], "you should use same size data"
     dice = []
     for i in range(y_true.shape[0]):
         dice.append(dice_coef(y_true[i], y_pred[i]))
     print(np.mean(dice))
+    
     return dice
